5d728f12ca3dfe10d9e12e374a4fb9c94e8a59bd
Alexander Lay <alexanderqlay@gmail.com>
2024-11-11 23:21:14 -0500

    examples: Add basic example of turning on/off each channel
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

---
+++ examples/SanityCheck/SanityCheck.ino
@@ -0,0 +1,22 @@
+#include <TLC59116.h>
+
+#define RESET_PIN D5
+
+TLC59116 board(0b1100000);
+
+void setup() {
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, LOW);
+  delay(5);
+  digitalWrite(RESET_PIN, HIGH);
+  
+  board.begin();
+}
+
+void loop() {
+  for (uint8_t channel; channel < NUM_CHANNELS; channel++) {
+    board.setBrightness(channel, 255);
+    delay(10);
+    board.setBrightness(channel, 0);
+  }
+}
