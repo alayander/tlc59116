cdc71667cfe60739b4bc8f88a66c037f964e1eb9
Alexander Lay <alexanderqlay@gmail.com>
2025-02-01 16:03:28 -0500

    driver: Refactor code to align with clang-format
    Refactors TLC59116.cpp, TLC59116.h, TLC59116Manager.cpp, TLC59116.h
    
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- src/TLC59116.cpp
+++ src/TLC59116.cpp
@@ -1,61 +1,61 @@
 /*
-    TLC59116.cpp - Library to interface with Texas Instruments TLC59116
-    I2C bus controlled 16-channel LED driver
-    https://github.com/alayander/tlc59116
+	TLC59116.cpp - Library to interface with Texas Instruments TLC59116
+	I2C bus controlled 16-channel LED driver
+	https://github.com/alayander/tlc59116
 */
 
 #include "TLC59116.h"
 
 TLC59116::TLC59116(uint8_t addr) {
-    _addr = addr;
-    _enable_shadow_registers = true;
+	_addr = addr;
+	_enable_shadow_registers = true;
 }
 
 TLC59116::TLC59116(uint8_t addr, bool enable_shadow_registers) {
-    _addr = addr;
-    _enable_shadow_registers = enable_shadow_registers;
+	_addr = addr;
+	_enable_shadow_registers = enable_shadow_registers;
 }
 
 void TLC59116::begin() {
-    /* Set MODE1 register to default values except bit [4] (OSC bit) */
-    /* According to spec sheet, proper operation requires OSC bit to be set low */
-    writeToReg(MODE1, 0x01);
-    /* At least 500us delay are required after setting OSC bit */
-    delay(1);
-    /* Set MODE2 register to default values */
-    writeToReg(MODE2, 0x00);
-    /* Enable each channel to be driven by individual PWM */
-    writeToReg(LEDOUT0, 0xAA);
-    writeToReg(LEDOUT1, 0xAA);
-    writeToReg(LEDOUT2, 0xAA);
-    writeToReg(LEDOUT3, 0xAA);
-    /* Initialize all channels to 0 (off) */
-    for (uint8_t channel = 0; channel < NUM_CHANNELS; channel++) {
-        setBrightness(channel, 0x00);
-        _shadow_registers[channel] = 0x00;
-    }
+	/* Set MODE1 register to default values except bit [4] (OSC bit) */
+	/* According to spec sheet, proper operation requires OSC bit to be set low */
+	writeToReg(MODE1, 0x01);
+	/* At least 500us delay are required after setting OSC bit */
+	delay(1);
+	/* Set MODE2 register to default values */
+	writeToReg(MODE2, 0x00);
+	/* Enable each channel to be driven by individual PWM */
+	writeToReg(LEDOUT0, 0xAA);
+	writeToReg(LEDOUT1, 0xAA);
+	writeToReg(LEDOUT2, 0xAA);
+	writeToReg(LEDOUT3, 0xAA);
+	/* Initialize all channels to 0 (off) */
+	for (uint8_t channel = 0; channel < NUM_CHANNELS; channel++) {
+		setBrightness(channel, 0x00);
+		_shadow_registers[channel] = 0x00;
+	}
 }
 
 void TLC59116::setPattern(uint16_t pattern, uint8_t brightness) {
-    for (uint8_t channel = 0; channel < NUM_CHANNELS; channel++) {
-        if (pattern & (1 << channel)) {
-            setBrightness(channel, brightness);
-        } else {
-            setBrightness(channel, 0x00);
-        }
-    }
+	for (uint8_t channel = 0; channel < NUM_CHANNELS; channel++) {
+		if (pattern & (1 << channel)) {
+			setBrightness(channel, brightness);
+		} else {
+			setBrightness(channel, 0x00);
+		}
+	}
 }
 
 void TLC59116::setBrightness(uint8_t channel, uint8_t brightness) {
-    if (!_enable_shadow_registers || _shadow_registers[channel] != brightness) {
-        writeToReg(PWM0 + (channel & 0x0F), brightness);
-        _shadow_registers[channel] = brightness;
-    }
+	if (!_enable_shadow_registers || _shadow_registers[channel] != brightness) {
+		writeToReg(PWM0 + (channel & 0x0F), brightness);
+		_shadow_registers[channel] = brightness;
+	}
 }
 
 void TLC59116::writeToReg(uint8_t reg, uint8_t val) {
-    Wire.beginTransmission(_addr);
-    Wire.write((char) reg);
-    Wire.write((char) val);
-    Wire.endTransmission();
+	Wire.beginTransmission(_addr);
+	Wire.write((char)reg);
+	Wire.write((char)val);
+	Wire.endTransmission();
 }
--- src/TLC59116.h
+++ src/TLC59116.h
@@ -1,7 +1,7 @@
 /*
-    TLC59116.h - Library to interface with Texas Instruments TLC59116
-    I2C bus controlled 16-channel LED driver
-    https://github.com/alayander/tlc59116
+	TLC59116.h - Library to interface with Texas Instruments TLC59116
+	I2C bus controlled 16-channel LED driver
+	https://github.com/alayander/tlc59116
 */
 
 #ifndef TLC59116_h
@@ -13,56 +13,56 @@
 #define NUM_CHANNELS 16
 
 /* Register Defintions */
-#define MODE1       0x00
-#define MODE2       0x01
-#define PWM0        0x02
-#define PWM1        0x03
-#define PWM2        0x04
-#define PWM3        0x05
-#define PWM4        0x06
-#define PWM5        0x07
-#define PWM6        0x08
-#define PWM7        0x09
-#define PWM8        0x0A
-#define PWM9        0x0B
-#define PWM10       0x0C
-#define PWM11       0x0D
-#define PWM12       0x0E
-#define PWM13       0x0F
-#define PWM14       0x10
-#define PWM15       0x11
-#define GRPPWM      0x12
-#define GRPFREQ     0x13
-#define LEDOUT0     0x14
-#define LEDOUT1     0x15
-#define LEDOUT2     0x16
-#define LEDOUT3     0x17
-#define SUBADR1     0x18
-#define SUBADR2     0x19
-#define SUBADR3     0x1A
-#define ALLCALLADR  0x1B
-#define IREF        0x1C
-#define EFLAG1      0x1D
-#define EFLAG2      0x1E
+#define MODE1 0x00
+#define MODE2 0x01
+#define PWM0 0x02
+#define PWM1 0x03
+#define PWM2 0x04
+#define PWM3 0x05
+#define PWM4 0x06
+#define PWM5 0x07
+#define PWM6 0x08
+#define PWM7 0x09
+#define PWM8 0x0A
+#define PWM9 0x0B
+#define PWM10 0x0C
+#define PWM11 0x0D
+#define PWM12 0x0E
+#define PWM13 0x0F
+#define PWM14 0x10
+#define PWM15 0x11
+#define GRPPWM 0x12
+#define GRPFREQ 0x13
+#define LEDOUT0 0x14
+#define LEDOUT1 0x15
+#define LEDOUT2 0x16
+#define LEDOUT3 0x17
+#define SUBADR1 0x18
+#define SUBADR2 0x19
+#define SUBADR3 0x1A
+#define ALLCALLADR 0x1B
+#define IREF 0x1C
+#define EFLAG1 0x1D
+#define EFLAG2 0x1E
 
 class TLC59116 {
-    private:
-        uint8_t _addr;
-        uint8_t _shadow_registers[16];
-        bool _enable_shadow_registers;
-        /* Write value to a register */
-        void writeToReg(uint8_t reg, uint8_t val);
-        
-    public:
-        /* Constructors */
-        TLC59116(uint8_t addr);
-        TLC59116(uint8_t addr, bool _enable_shadow_registers);
-        /* Initialize the driver and set all channels to default (0) */
-        void begin();
-        /* Set bits given by binary pattern to brightness value (0-255) */
-        void setPattern(uint16_t pattern, uint8_t brightness);
-        /* Set brightness value (0-255) for a given channel (0-15) */
-        void setBrightness(uint8_t channel, uint8_t brightness);
+  private:
+	uint8_t _addr;
+	uint8_t _shadow_registers[16];
+	bool _enable_shadow_registers;
+	/* Write value to a register */
+	void writeToReg(uint8_t reg, uint8_t val);
+
+  public:
+	/* Constructors */
+	TLC59116(uint8_t addr);
+	TLC59116(uint8_t addr, bool _enable_shadow_registers);
+	/* Initialize the driver and set all channels to default (0) */
+	void begin();
+	/* Set bits given by binary pattern to brightness value (0-255) */
+	void setPattern(uint16_t pattern, uint8_t brightness);
+	/* Set brightness value (0-255) for a given channel (0-15) */
+	void setBrightness(uint8_t channel, uint8_t brightness);
 };
 
 #endif
