6951738755d9fa5d39a0e01c572ac1d2157bff3e
Alexander Lay <alexanderqlay@gmail.com>
2025-02-01 16:03:28 -0500

    driver: Refactor code to align with clang-format
    Refactors TLC59116.cpp, TLC59116.h, TLC59116Manager.cpp, TLC59116.h
    
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- src/TLC59116Manager.h
+++ src/TLC59116Manager.h
@@ -1,7 +1,7 @@
 /*
-    TLC59116Manager.h - Library to manage multiple
-    Texas Instruments TLC59116 I2C bus controlled 16-channel LED driver
-    https://github.com/alayander/tlc59116
+	TLC59116Manager.h - Library to manage multiple
+	Texas Instruments TLC59116 I2C bus controlled 16-channel LED driver
+	https://github.com/alayander/tlc59116
 */
 
 #ifndef TLC59116Manager_h
@@ -12,19 +12,19 @@
 #include <Wire.h>
 
 class TLC59116Manager {
-    private:
-        TLC59116* _drivers[4];
-        size_t _count;
+  private:
+	TLC59116 *_drivers[4];
+	size_t _count;
 
-    public:
-        /* Constructor */
-        TLC59116Manager();
-        /* Add driver to manager */
-        bool add(TLC59116* driver);
-        /* Initialize all drivers and set all channels to default (0) */
-        void begin();
-        /* Set bits given by binary pattern to brightness value (0-255) */
-        void setPattern(uint64_t pattern, uint8_t brightness);
+  public:
+	/* Constructor */
+	TLC59116Manager();
+	/* Add driver to manager */
+	bool add(TLC59116 *driver);
+	/* Initialize all drivers and set all channels to default (0) */
+	void begin();
+	/* Set bits given by binary pattern to brightness value (0-255) */
+	void setPattern(uint64_t pattern, uint8_t brightness);
 };
 
 #endif
